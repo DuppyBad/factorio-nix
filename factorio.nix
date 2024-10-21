a701a2f2554455e8cb51c445d14210d72b290bb0
kyrios <duppybad@proton.me>
2024-10-21 23:52:19 +0100

    dev:refactor factorio.nix

--- factorio.nix
+++ factorio.nix
@@ -1,16 +1,35 @@
-{
-  config,
-  pkgs,
-  inputs,
-  ...
-}: {
-  nixpkgs.config.allowUnfree = true;
-  environment.systemPackages = with pkgs; [
-    factorio-headless
-  ];
+{ pkgs, config, inputs, ...}:
+  let
+    sshKeys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBU9ecVSTJ6nbhXtJS6A/yutUhOAom+OSVU8SBzayHFf kyrios@machina"
+    ];
 
-  services.factorio = {
-    enable = true;
-    openFirewall = true;
-  };
-}
+    host = "factorio-server.main.factorio-nix.DuppyBad.garnix.me";
+  in
+  {
+    garnix.server.enable = true;
+    nixpkgs.config.allowUnfree = true;
+    services = {
+      openssh.enable = true;
+      factorio = {
+        enable = true;
+        openFirewall = true;
+      };
+    };
+    
+
+    users.users.me = {
+      isNormalUser = true;
+      description = "me";
+      extraGroups = ["wheel" "systemd-journal"];
+      openssh.authorizedKeys.keys = sshKeys;
+    };
+
+    security.sudo.wheelNeedsPassword = false;
+
+    environment.systemPackages = with pkgs; [
+      factorio-headless
+      htop
+    ];
+  }
+    
--- flake.nix
+++ flake.nix
@@ -3,16 +3,15 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable"; # Use the unstable channel for the latest packages
+    garnix-lib.url = "github:garnix-io/garnix-lib";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-  }: {
+  outputs = inputs: {
     nixosConfigurations = {
       factorio-server = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux"; # Specify your system architecture
         modules = [
+          inputs.garnix-lib.nixosModules.garnix
           ./factorio.nix
           ./config.nix
         ];
