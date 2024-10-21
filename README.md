18202f54e3b157c8e884995e5493faaaa5d94224
kyrios <duppybad@proton.me>
2024-10-22 00:56:44 +0100

    meta:updated readme, addded strings to factorio entry

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # factorio-nix
-Continuous deployment Factorio
+Continuous deployment Factorio via Garnix
+
+- Fully Public(Maybe we should change that)
+- Fully Open(Doesn't care if you have a factorio account)
+- Not persistent (TODO: Persistence)
+
+
--- factorio.nix
+++ factorio.nix
@@ -4,7 +4,7 @@
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBU9ecVSTJ6nbhXtJS6A/yutUhOAom+OSVU8SBzayHFf kyrios@machina"
     ];
 
-    host = "factorio-server.main.factorio-nix.DuppyBad.garnix.me";
+    #host = "factorio-server.main.factorio-nix.DuppyBad.garnix.me"; # For self-references in web hosting
   in
   {
     garnix.server.enable = true;
@@ -14,6 +14,8 @@
       factorio = {
         enable = true;
         openFirewall = true;
+        description = "Tinybrain gaming?";
+        game-name = "TinyFactory";
       };
     };
     
@@ -30,6 +32,8 @@
     environment.systemPackages = with pkgs; [
       factorio-headless
       htop
+      bottom
     ];
+    networking.firewall.allowedUDPPorts = [34197]; #Just in case, will check later if needed
   }
     
